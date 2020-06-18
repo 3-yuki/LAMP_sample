afbe4590da7f06643d1be4a82ce02ab5428ecc13
3-yuki <y.mikami.sys19@morijyobi.ac.jp>
2020-06-18 11:45:24 +0900

    add index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+$dsn = 'mysql:dbname=user;host=localhost;';
+$user = 'mikami';
+$password = 'morijyobi';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+
+    $sql = "select * from user"
+    $result = $dbh->query($sql);
+
+} catch (PDOException $e) {
+    echo "接続失敗: " . $e->getMessage() . "\n";
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LAMP SAMPLE PAGE</title>
+</head>
+<body>
+ <div class="container">
+    <?php foreach( $result as $value) {
+        echo "<h1>$value[id] $value[name] $value[age]</h1>";
+    }?>
+ </div>   
+</body>
+</html>
