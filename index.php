9aef467bb5a3b11f0acc8d20f6e84d5e0402f692
3-yuki <y.mikami.sys19@morijyobi.ac.jp>
2020-06-25 11:21:06 +0900

    update

--- index.php
+++ index.php
@@ -104,6 +104,31 @@ try {
           <button type="submit" class="btn btn-primary">Insert</button>
       </form>
     </div>
+
+    <div id ="tab3" class="tab-pane ">
+            <form class="mt-5" action="./update.php" method="POST">
+               <div class="form-group">
+                 <label for="id">ID</label>
+                 <input class="form-control" type="text" name="id" id="id">
+               </div>
+            </form>
+
+        </div>
+          <form action="">
+        <div class="form-group">
+          <label for="name">Your Name</label>
+          <input class="form-control" type="text" name="name" id="name">
+        </div>
+
+        <div class="form-group">
+          <label for="age">Age</label>
+          <input class="form-control" type="text" name="age" id="age">
+        </div>
+
+          <button type="submit" class="btn btn-primary">Update</button>
+      </form>
+    </div>
+
   </div>
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
